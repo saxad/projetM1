6b2d073bf94edd60bf4c8e881b83721fe50d396f
saxad <saad_zizi@hotmail.com>
2019-02-05 18:12:21 +0100

    detections des contours

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import cv2
 import matplotlib.pyplot as plt
 import argparse
+import imutils
+from imutils.perspective  import four_point_transform
 
 
 argObj = argparse.ArgumentParser()
@@ -17,22 +19,41 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray,(5,5), 0)
 
 edged = cv2.Canny(blurred, 75, 200)
-plt.imshow(edged, cmap='gray')
-plt.show()
+
+
+
+
+
+
+#recupérer les contours externes de la page puis garde ses 4 pts
 cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
 	cv2.CHAIN_APPROX_SIMPLE)
+
+
+
 cnts = imutils.grab_contours(cnts)
 docCnt = None
 
 if len(cnts) > 0:
-	
-	cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
-	for c in cnts:
-		
-		peri = cv2.arcLength(c, True)
-		approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-		if len(approx) == 4:
-			docCnt = approx
-			break
+
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+    for c in cnts:
+
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+        #si on trouve les 4 points c'est qu'on trouvé la photo
+        if len(approx) == 4:
+            docCnt = approx
+            break
+
 paper = four_point_transform(image, docCnt.reshape(4, 2))
 warped = four_point_transform(gray, docCnt.reshape(4, 2))
+
+thresh = cv2.threshold(warped, 0, 255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+questionCnts = []
+
+
+plt.imshow(paper, cmap='gray')
+plt.show()
